4ff4761721c737d24cfb6c8aa444c60258b7525c
Sreekanth Boddireddy <sreekanth.boddireddy@autodesk.com>
2017-06-14 15:11:48 -0700

    add moment.js library

--- scripts.js
+++ scripts.js
@@ -1,13 +1,6 @@
-function formatDigits(num) {
-  if(num <= 9){
-    num = '0' + num;
-  }
-  return num;
-}
 function showTime() {
-  var date = new Date();
-  var timeFormatted =  formatDigits(date.getHours())+ ":" + formatDigits(date.getMinutes()) + ":" + formatDigits(date.getSeconds());
-  document.getElementById('time').innerHTML = timeFormatted;
+  document.getElementById('time').innerHTML = moment().format('HH:mm');;
+  document.getElementById('date').innerHTML = moment().format('MMMM Do YYYY');;
 }
 document.addEventListener("DOMContentLoaded", function(event) { 
   setInterval(showTime, 1000);
