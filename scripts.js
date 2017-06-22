0da650d594b96bd0f4f5526eec5223cc5981cd15
Sreekanth Boddireddy <sreekanth.boddireddy@autodesk.com>
2017-06-21 20:41:32 -0700

    change image on double click

--- scripts.js
+++ scripts.js
@@ -2,6 +2,13 @@ function showTime() {
   document.getElementById('time').innerHTML = moment().format('HH:mm');;
   document.getElementById('date').innerHTML = moment().format('MMMM Do YYYY');;
 }
+function getRandomIntInclusive() {
+  var totalImages = 6;
+  return Math.floor(Math.random() * (totalImages)) + 1;
+}
 document.addEventListener("DOMContentLoaded", function(event) { 
   setInterval(showTime, 1000);
+});
+document.addEventListener('dblclick', function() {
+    document.body.style.backgroundImage = "url('images/"+ getRandomIntInclusive() + ".jpg')";
 });
