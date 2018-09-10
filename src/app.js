6e9a67bd002628be223b5290112485d018025f93
sreekanth boddireddy <happysreekanth@gmail.com>
2018-09-09 19:24:41 -0700

    separate components and remove todo list for now

--- src/app.js
+++ src/app.js
@@ -1,68 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Moment from 'moment';
 import {TabbyClock} from '../src/components/clock';
-import {TabbyTodoList} from '../src/components/todo'
 
 require('./styles/tabby.less');
 
 class TabbyApp extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { items: [], text: '', clock:{time: Moment().format('HH:mm:ss'), date: Moment().format('MMMM Do YYYY')}};
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+        this.state = { };
     }
 
     render() {
         return (
             <div className="tabby-container">
                 <TabbyClock clock={this.state.clock} />
-                <div className="tabby-todo">
-                    <h3>TODO</h3>
-                    <TabbyTodoList items={this.state.items} />
-                    <form onSubmit={this.handleSubmit}>
-                        <input id="new-todo" onChange={this.handleChange} value={this.state.text}/>
-                        <button>Add #{this.state.items.length + 1}</button>
-                    </form>
-                </div>
             </div>
         );
-    }
-
-    tick() {
-        this.setState(prevState => ({
-            clock: {
-                time: Moment().format('HH:mm:ss'),
-                date: Moment().format('MMMM Do YYYY')
-        }}));
-    }
-
-    componentDidMount() {
-        this.interval = setInterval(() => this.tick(), 1000);
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.interval);
-    }
-
-    handleChange(e) {
-        this.setState({ text: e.target.value });
-    }
-
-    handleSubmit(e) {
-        e.preventDefault();
-        if (!this.state.text.length) {
-            return;
-        }
-        const newItem = {
-            text: this.state.text,
-            id: Date.now()
-        };
-        this.setState(prevState => ({
-            items: prevState.items.concat(newItem),
-            text: ''
-        }));
     }
 }
 
--- src/components/clock.js
+++ src/components/clock.js
@@ -1,14 +1,40 @@
 import React from 'react';
+import Moment from 'moment';
 
 export class TabbyClock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            clock: {
+                time: Moment().format('HH:mm:ss'),
+                date: Moment().format('MMMM Do YYYY')
+            }};
+    }
+
+    tick() {
+        this.setState(prevState => ({
+            clock: {
+                time: Moment().format('HH:mm:ss'),
+                date: Moment().format('MMMM Do YYYY')
+            }}));
+    }
+
+    componentDidMount() {
+        this.interval = setInterval(() => this.tick(), 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
+
     render(){
         return (
             <div className="tabby-clock">
                 <div className="tabby-clock-time">
-                    {this.props.clock.time}
+                    {this.state.clock.time}
                 </div>
                 <div className="tabby-clock-date">
-                    {this.props.clock.date}
+                    {this.state.clock.date}
                 </div>
             </div>
         )
